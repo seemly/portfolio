1cac3f6ec5b201ebec629798abecb92efdb9c161
Chris Sparshott <idioteque23@gmail.com>
2018-11-12 19:14:54 +0000

    tidy

--- src/Application/Infrastructure/Meta/Personal.php
+++ src/Application/Infrastructure/Meta/Personal.php
@@ -6,15 +6,15 @@ class Personal
 {
   const JOB_SEEKING = true;
 
-  const NAME = 'Chris Sparshott';
+  const NAME      = 'Chris Sparshott';
   const JOB_TITLE = 'PHP Web Developer & Frontend Developer';
 
   const MOBILE = '07572 460 634';
-  const EMAIL = 'chris@seemly.co.uk';
+  const EMAIL  = 'chris@seemly.co.uk';
 
   const DOMAIN_PRICES = 'http://seemly.co.uk/domains/prices';
-  const PORTFOLIO = 'http://sparshott.co.uk';
-  const TWITTER = 'https://twitter.com/seemly';
-  const GITHUB = 'https://github.com/seemly';
-  const LINKEDIN = 'https://www.linkedin.com/in/chris-sparshott';
+  const PORTFOLIO     = 'http://sparshott.co.uk';
+  const TWITTER       = 'https://twitter.com/seemly';
+  const GITHUB        = 'https://github.com/seemly';
+  const LINKEDIN      = 'https://www.linkedin.com/in/chris-sparshott';
 }
