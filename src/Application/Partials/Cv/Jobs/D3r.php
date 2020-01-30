46878cd68dc703b4ed61fd09cfd7cb3088f9c390
Chris Sparshott <idioteque23@gmail.com>
2020-01-30 12:22:53 +0000

    D3R cv updates

--- src/Application/Partials/Cv/Jobs/D3r.php
+++ src/Application/Partials/Cv/Jobs/D3r.php
@@ -4,7 +4,7 @@ namespace App\Application\Partials\Cv\Jobs;
 
 use Packaged\Glimpse\Core\HtmlTag;
 use Packaged\Glimpse\Tags\Lists\UnorderedList;
-use Packaged\Glimpse\Tags\Text\BoldText;
+use Packaged\Glimpse\Tags\Text\HeadingFive;
 use Packaged\Glimpse\Tags\Text\Paragraph;
 
 class D3r extends AbstractCvJobItem
@@ -28,34 +28,35 @@ class D3r extends AbstractCvJobItem
   /**
    * @return array
    */
-  protected function _getImplementations()
+  protected function _getMaintenanceProjects()
   {
-    $list = UnorderedList::create();
+    $fine = $this->_createOutboundLink('Fine', 'http://finexfine.com');
 
-    if($list->getContent())
-    {
-      return [
-        Paragraph::create(BoldText::create('Implementations;')),
-        $list,
-      ];
-    }
-    return null;
-  }
-
-  /**
-   * @return array
-   */
-  protected function _getProjects()
-  {
     $list = UnorderedList::create();
-    if($list->getContent())
-    {
-      return [
-        Paragraph::create(BoldText::create('Projects;')),
-        $list,
-      ];
-    }
-    return null;
+    $list->addItem($fine);
+    $list->addItem($this->_createOutboundLink('Gemini Data Loggers', 'https://www.geminidataloggers.com'));
+    $list->addItem($this->_createOutboundLink('The Beaumont', 'https://www.thebeaumont.com'));
+    $list->addItem($this->_createOutboundLink('Aspinal', 'https://www.aspinaloflondon.com'));
+    $list->addItem($this->_createOutboundLink('West Dean College', 'https://www.westdean.org.uk'));
+
+    return [
+      HeadingFive::create('Daily Tasks'),
+      Paragraph::create(
+        'During my time at D3R I worked within the support and maintenance team, implementing bug fixes and new features.'
+      ),
+      Paragraph::create(
+        [
+          'One of the larger pieces of work I completed was for ',
+          $fine,
+          ', a membership only website with a highly exclusive product range. ' .
+          'I was tasked with updating the home page to promote the type of products available for purchase, ' .
+          'and allowing prospective members to read selected featured articles to entice membership applications.'
+        ]
+      ),
+      HeadingFive::create('Client Projects'),
+      Paragraph::create('The following is a list of client websites where I would fix bugs and add new functionality as requested:'),
+      $list,
+    ];
   }
 
   /**
@@ -64,11 +65,7 @@ class D3r extends AbstractCvJobItem
   protected function _description()
   {
     return [
-      Paragraph::create(
-        'Daily tasks include website builds, maintenance, updates, code refactoring, etc.'
-      ),
-      $this->_getImplementations(),
-      $this->_getProjects(),
+      $this->_getMaintenanceProjects(),
     ];
   }
 
